12465dac1ba67d6c0f60687423bb39b676609c32
Quentin Fontaine <quentinfontaine7.qf@gmail.com>
2015-11-12 15:15:11 +0100

    Ajout des trois premières histoires

--- src/Engine.js
+++ src/Engine.js
@@ -7,27 +7,75 @@ var Engine = function () {
     var couleur = {black: 0, white: 1, red: 2, green: 3, blue:4, yellow: 5};
     var joueurCourant = 1;
 
+    var joueurPieces = [[0,0,0,0,0,0],[0,0,0,0,0,0]];
+
     var initialisationPlateau = function(){
-        plateauDeJeu = [[color.black,color.green,color.white,color.blue,color.red,color.white],
-            [color.yellow,color.white,color.green, color.red, color.yellow, color.blue],
-            [color.blue, color.yellow, color.blue, color.white, color.black, color.red],
-            [color.red, coor.black, color.red, color.green, color.blue, color.white],
-            [color.white, color.green, color.yellow, color.black, color.yellow, color.green],
-            [color.yellow, color.blue, color.black, color.red, color.green, color.black]];
+        plateauDeJeu = [[couleur.black,couleur.green,couleur.white, couleur.blue, couleur.red, couleur.white],
+            [couleur.yellow, couleur.white, couleur.green, couleur.red, couleur.yellow, couleur.blue],
+            [couleur.blue, couleur.yellow, couleur.blue, couleur.white, couleur.black, couleur.red],
+            [couleur.red, couleur.black, couleur.red, couleur.green, couleur.blue, couleur.white],
+            [couleur.white, couleur.green, couleur.yellow, couleur.black, couleur.yellow, couleur.green],
+            [couleur.yellow, couleur.blue, couleur.black, couleur.red, couleur.green, couleur.black]];
         return 0;
     };
 
+    // Transforme i et j en caracteres
+    var getChar = function (str) {
+        var i = str.charCodeAt(0) - "A".charCodeAt(0);
+        var j = str.charCodeAt(1) - "1".charCodeAt(0);
+
+        return {"i" : i, "j" : j};
+    };
     //Public methods
 
-    this.initialisation = function(){
-        return (initialisationPlateau()===0) ? true : false;
+    var init  = function(){
+        initialisationPlateau();
     };
 
-    this.getJoueurCourant = function(){
-        return getJoueur(joueurCourant);
+    this.validPlateau = function(){
+      var i, j ;
+        for(i=0;i<5 ; i++){
+            for(j=0; j<5;j++){
+                if(plateauDeJeu[i][j] === plateauDeJeu[i][j+1] || plateauDeJeu[i][j] === plateauDeJeu[i+1][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
     };
 
-    var getJoueur =function(joueur){
-        return (joueur == 1)? "white":"black";
+    this.jouerCoup = function (str) {
+        var coup = getChar(str);
+        if (coup.i <= 6 && coup.j <= 6) {
+            var color = plateauDeJeu[coup.j][coup.i];
+            plateauDeJeu[coup.j][coup.i] = -1;
+            joueurPieces[joueurCourant - 1][color]++;
+        }
     };
+
+    // Pieces dont on dispose l'indice
+    this.case = function (str) {
+        var coup = getChar(str);
+        return plateauDeJeu[coup.j][coup.i];
+    };
+
+    //Nombre de pieces sur le plateau
+    this.nbPieces = function () {
+        var i, j, cpt = 0;
+        for (i = 0; i < 6; i++) {
+            for (j = 0; j < 6; j++) {
+                if (plateauDeJeu[i][j] !== -1) {
+                    cpt++;
+                }
+            }
+        }
+        return cpt;
+    };
+
+    // Pieces prises par le joueur lors de son coup + couleur
+    this.nbCouleurPrisesJoueur = function (joueur, c) {
+        return joueurPieces[joueur - 1][couleur[c]];
+    };
+
+    init();
 };
--- test/test.js
+++ test/test.js
@@ -5,11 +5,19 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 //Test d'initialisation du plateau
 PalettoTestCase.prototype.testStory1 = function () {
     var e = new Engine();
-    assertTrue(e.initialisation());
+    assertTrue(e.validPlateau());
 };
 
-//Test
-PalettoTestCase.prototype.testStory2 = function() {
-    var e= new Engine();
-    assertTrue(e.getJoueurCourant() == "blanc");
+//Test si la case A6 a été joué
+PalettoTestCase.prototype.testStory2 = function () {
+    var e = new Engine();
+    e.jouerCoup("A6");
+};
+
+PalettoTestCase.prototype.testStory3 = function () {
+    var e = new Engine();
+    e.jouerCoup("A6");
+    assertEquals(e.case("A6"),-1);
+    assertEquals(e.nbPieces(),35);
+    assertEquals(e.nbCouleurPrisesJoueur(1,"yellow"),1);
 };
